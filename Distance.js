5c27dea065d9189e44a9a53e7c255fae91d0b709
JulianMorales1 <98353349+JulianMorales1@users.noreply.github.com>
2022-02-06 21:45:01 -0500

    Create Distance.js

---
+++ Distance.js
@@ -0,0 +1,12 @@
+let x1 = 2;
+let x2 = 4;
+let y1 = 3;
+let y2 = 5;
+
+let x = x2 - x1;
+let y = y2 - y1;
+
+    
+let dist = Math.sqrt(x * x + y * y);
+
+console.log("The distance between the two points is " + dist + ".");
